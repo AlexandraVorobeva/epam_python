faea5808e57f42e0809cbd7568048ce55851f78f
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-03-10 21:37:39 +0300

    3 hw

---
+++ hw_3/task01/task01.py
@@ -0,0 +1,34 @@
+def cache(times: int):
+    """
+    This function accepts any function for cache
+    and gives out cached value up to times number only.
+
+    Args:
+        times: number
+
+    Returns:
+        decorated function: callable
+
+    """
+    cacher = {}
+
+    def wrapper(func):
+        def func_cache(*args):
+            if times > 0:
+                if args not in cacher:
+                    value = func(*args)
+                    cacher[args] = [value, 0]
+                    return value
+                elif cacher[args][1] < times:
+                    cacher[args][1] += 1
+                    return cacher[args][0]
+                else:
+                    value = func(*args)
+                    cacher[args] = [value, 0]
+                    return value
+            return func(*args)
+
+        return func_cache
+
+    return wrapper
+
