ce463e9c65f88d8c07c7ffbf1660c7a65b2d7c76
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-04-04 12:55:37 +0300

    update hw8

--- hw_8/tests/test_task1.py
+++ hw_8/tests/test_task1.py
@@ -2,38 +2,18 @@ import pytest
 from hw_8.task1 import KeyValueStorage
 
 
-@pytest.fixture()
-def file_name(data: str):
-    filename = "example.txt"
-    with open(filename, "w") as fl:
-        fl.write(data)
-    yield filename
+def test_KeyValueStorage_brackets():
+    actual_result = KeyValueStorage("task1.txt")
+    assert actual_result["name"] == "kek"
+    assert actual_result["last_name"] == "top"
 
 
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        ("1=something"),
-        ("1=1"),
-        ("Hello!=world!"),
-        ("return=return"),
-    ],
-)
-def test_value_error(file_name):
-    with pytest.raises(ValueError, match="Incorrect key!"):
-        KeyValueStorage(file_name)
+def test_KeyValueStorage_dot():
+    actual_result = KeyValueStorage("task1.txt")
+    assert actual_result.name == "kek"
+    assert actual_result.last_name == "top"
 
 
-@pytest.mark.parametrize(
-    ("data", "correct_storage"),
-    [
-        (
-            "name=kek last_name=top power=9001 song=shadilay",
-            {"name": "kek", "last_name": "top", "power": 9001, "song": "shadilay"},
-        ),
-        ("name=kek name=run", {"name": "kek"}),
-    ],
-)
-def test_key_value_storage(file_name, correct_storage):
-    storage = KeyValueStorage(file_name)
-    assert storage["name"] == storage.name == correct_storage["name"]
+def test_KeyValueStorage_error():
+    with pytest.raises(ValueError, match="Invalid key!"):
+        KeyValueStorage("example.txt")
